cc119de7f8df4e719065fe9b42bc8a08f771fc05
Cheng Xi Tsou <chengxi600@gmail.com>
2021-04-04 02:18:20 -0400

    commenting helpers and finish create ingredient

--- server/lib/cooking_app_web/controllers/ingredient_controller.ex
+++ server/lib/cooking_app_web/controllers/ingredient_controller.ex
@@ -18,33 +18,48 @@ defmodule CookingAppWeb.IngredientController do
   # Creates ingredient
   # ingredient_params = {"name", name}
   def create(conn, %{"ingredient" => ingredient_params}) do
-    ingredient = getIngredientByName(ingredient_params["name"]);
-    if(ingredient) do
-      # TODO: change this name params to the API result's name
-      db_ingredient = Ingredients.get_ingredient_by_name(ingredient_params["name"])
-      if(db_ingredient) do
-        # TODO: change this name params to the API result's name
-        case Ingredients.create_ingredient(ingredient_params) do
-          {:ok, result} ->
+    #check the result from API
+    case getIngredientByName(ingredient_params["name"]) do
+      #if API returns a valid resp
+      {:ok, result} ->
+        #if the list of possible ingredients are not empty
+        if(result != []) do
+          #convert list of ingredients into string names
+          ingr_list = Enum.map(result, fn ing -> ing["name"] end)
+          #Add item to db if it is not in db and return true, else false.
+          status_list = Enum.map(ingr_list, fn ing ->
+            db_ingredient = Ingredients.get_ingredient_by_name(ing)
+            if(!db_ingredient) do
+              params = %{ingredient_name: ing}
+              Ingredients.create_ingredient(params)
+              true
+            else
+              false
+            end
+          end)
+          #if status_list is all false, then that means all possible ingredients are already in the db
+          if Enum.all?(status_list, fn status -> !status end) do
+            conn
+            |> put_resp_header("content-type", "application/json; charset=UTF-8")
+            |> send_resp(:not_acceptable, Jason.encode!(%{error: "All variations of the ingredient are in the database."}))
+          else
             conn
             |> put_status(:created)
             |> put_resp_header("location", Routes.ingredient_path(conn, :show, ingredient))
             |> render("show.json", owned_ingredient: result)
-          {:error, %Ecto.Changeset{} = changeset} ->
-            conn 
-            |> put_resp_header("content-type", "application/json; charset=UTF-8")
-            |> send_resp(:not_modified, Jason.encode!(%{error: changeset.errors}))
-        end 
-      else 
+          end
+        #if results is empty
+        else 
+          conn
+          |> put_resp_header("content-type", "application/json; charset=UTF-8")
+          |> send_resp(:not_acceptable, Jason.encode!(%{error: "Given Ingredient Not Found."}))
+        end
+    #error from API
+    {:error, _} ->
         conn
         |> put_resp_header("content-type", "application/json; charset=UTF-8")
-        |> send_resp(:not_acceptable, Jason.encode!(%{error: "Given Ingredient Is Already In Database."}))
-      end 
-    else 
-      conn
-      |> put_resp_header("content-type", "application/json; charset=UTF-8")
-      |> send_resp(:not_acceptable, Jason.encode!(%{error: "Given Ingredient Not Found."}))
-    end 
+        |> send_resp(:not_acceptable, Jason.encode!(%{error: "Error with the API"}))
+    end
   end
 
   # To Be Decided
--- server/lib/cooking_app_web/helpers.ex
+++ server/lib/cooking_app_web/helpers.ex
@@ -1,14 +1,23 @@
 defmodule CookingAppWeb.Helpers do
 
+    #TODO make api key an environment variable
     def getApiKey() do
         "88a684f43c204ecea33e2e0b904f7565"
     end
 
-    # show recipe by id
+    #given recipe id, returns {:status, result} where 
+    #result = %{
+        #id: recipe id
+        #title: recipe name
+        #image: recipe image url
+        #sourceUrl: the url for the original recipe
+    #}
+    #error returns empty result
     def getRecipeById(id) do
         api_key = getApiKey()
         url = "https://api.spoonacular.com/recipes/716429/information?apiKey=#{api_key}&includeNutrition=true"
         resp = HTTPoison.get!(url)
+        #if 200 status code
         if resp.status_code == 200 do
             data = Jason.decode!(resp.body)
             result = %{
@@ -26,6 +35,8 @@ defmodule CookingAppWeb.Helpers do
     end
 
     # converts a list to a comma separated string array
+    # Input: [a, b, c, d, e, f]
+    # Output: "a,b,c,d,e,f"
     def arrToString(arr, accum) do
         if arr == [] do
             String.slice(accum, 0..-2)
@@ -35,11 +46,37 @@ defmodule CookingAppWeb.Helpers do
     end
 
     #converts spoonacular's list of ingredients dict to list of ingredient names
+    # Input: [%{
+        #id: ...
+        #name: "a"
+    #},
+    #%{
+        #id: ...
+        #name: "b"
+    #}]
+    #Output: [a, b]
+
     def cleanIngrList(data) do
         Enum.map(data, fn ing -> ing["name"] end)
     end
-    
-    # show receipe by an array of owned-ingredients
+
+
+    @doc """
+    show receipe by an array of owned-ingredients
+    Input: ["apple", "banana", "salt", "chicken"]
+    Output: {:status, result} where
+    result = [%{
+        id: recipe id
+        title: recipe name
+        image: image url of recipe
+        missedIngredients: list of missing ingredients 
+        usedIngredients: list of used ingredients
+        unusedIngredients: list of unused ingredients 
+    },
+    %{
+        ...
+    }]
+    """
     def getRecipeByIngredients(ing) do
         api_key = getApiKey()
         ing_string = arrToString(ing, "")
@@ -47,13 +84,13 @@ defmodule CookingAppWeb.Helpers do
         resp = HTTPoison.get!(url)
         if resp.status_code == 200 do
             data = Jason.decode!(resp.body)
-            result = %{
-                id: data["id"],
-                title: data["title"],
-                image: data["image"],
-                missedIngredients: cleanIngrList(data["missedIngredients"]),
-                usedIngredients: cleanIngrList(data["usedIngredients"]),
-                unusedIngredients: cleanIngrList(data["unusedIngredients"])
+            result = Enum.map(data, fn rcpe -> %{
+                id: rcpe["id"],
+                title: rcpe["title"],
+                image: rcpe["image"],
+                missedIngredients: cleanIngrList(rcpe["missedIngredients"]),
+                usedIngredients: cleanIngrList(rcpe["usedIngredients"]),
+                unusedIngredients: cleanIngrList(rcpe["unusedIngredients"])
             }
             {:ok, result}
         else
@@ -62,7 +99,18 @@ defmodule CookingAppWeb.Helpers do
         end
     end
 
-    # show a single ingredient by name ~~~
+    @doc """
+    show a single ingredient by name ~~~
+    {:status, result} where
+    result = [%{
+        id: ingredient id
+        name: ingredient name
+        image: ingredient image
+    },
+    %{
+        ...
+    }]
+    """
     def getIngredientByName(name) do
         api_key = getApiKey()
         url = "https://api.spoonacular.com/food/ingredients/search?apiKey=#{api_key}&query=#{name}"
